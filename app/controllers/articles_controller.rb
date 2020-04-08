1ec83b006bdce3faf2dbbf7833853ac9391c327f
abdelp <juniorperezpy@gmail.com>
2020-04-08 15:53:36 -0400

    Sorcery authentication complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  # before_action :require_login, except: [:new, :create, :edit, :update, :destroy]
+
   include ArticlesHelper
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,12 @@ Rails.application.routes.draw do
 
   resources :tags
 
+  resources :authors
+
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+
+  get 'login' => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
